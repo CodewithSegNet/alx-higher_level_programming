d8c432685206a5c5cb78b5056fd16b5632a3f3f9
Omogbehinmi Olusegun Emmanuel <eeomo@yahoo.com>
2022-12-20 16:10:33 -0800

    fix 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Square generation module for Python project 0x06
+"""
+
+
 class Square:
-    """Empty class that defines a square"""
+    """class defined for square generation
+    """
     pass
