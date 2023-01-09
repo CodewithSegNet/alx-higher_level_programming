d1261648c60c3f96b0fd9071652341326da6a86c
Omogbehinmi Olusegun Emmanuel <eeomo@yahoo.com>
2023-01-09 15:28:22 -0800

    added new files

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-===========================
-Module with class MyList
-===========================
-"""
+"""This module inherits from the list class"""
 
 
 class MyList(list):
-    """Class with method print_sorted"""
-    pass
-
+    """A class that inherits from list"""
     def print_sorted(self):
-        """Methot that sorted a list"""
-
-        print(sorted(list(self)))
+        """prints a sorted list"""
+        print(sorted(self))
