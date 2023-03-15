571cb09b5f00b27e445d300565bc80a7ed7225a9
segun-alx <eeomo@yahoo.com>
2023-03-14 18:20:37 -0700

    update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-// JS FUNCTION TO REVERSE A LIST
 exports.esrever = function (list) {
-  let newList = [];
-  for (let index = list.length - 1; index >= 0; index--) {
-    newList.push(list[index]);
-  }
-  return newList;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
