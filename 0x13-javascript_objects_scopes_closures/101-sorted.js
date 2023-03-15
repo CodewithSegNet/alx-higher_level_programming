dffa02b61b5ebe5de07a94e61aed7b823819f637
segun-alx <eeomo@yahoo.com>
2023-03-14 18:41:22 -0700

    update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
-}
-console.log(newDict);
+
+const { dict } = require('./101-data');
+
+const convertedArr = Object.entries(dict);
+
+const newObj = {};
+
+convertedArr.forEach(element => {
+  newObj[element[1]] ? newObj[element[1]].push(element[0]) : newObj[element[1]] = [element[0]];
+});
+
+console.log(newObj);
