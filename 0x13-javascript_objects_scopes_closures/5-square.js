04d5e2a92c3f567e1f19bd5ab5a4cb21837bcf78
segun-alx <eeomo@yahoo.com>
2023-03-14 18:19:07 -0700

    update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,33 +1,6 @@
 #!/usr/bin/node
-// JS script to define a class, extends to another class
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this;
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    for (let count = 0; count < this.height; count++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () {
-    let holder = this.width;
-    this.width = this.height;
-    this.height = holder;
-  }
-  double () {
-    this.width = 2 * this.width;
-    this.height = 2 * this.height;
-  }
-}
-
-class Square extends Rectangle {
+module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
-}
-module.exports = Square;
+};
