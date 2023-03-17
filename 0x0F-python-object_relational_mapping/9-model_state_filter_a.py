484eacbcb79a2a1b8b80e58191a4439163ebf278
emmanuel <eeomo@yahoo.com>
2023-03-17 04:37:48 -0700

    9-model_state_filter_a.py

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script lists all objects that contain the letter a
+Takes three arguments
+    mysql username
+    mysql password
+    database name
+Connects to host localhost and default port (3306)
+"""
+if __name__ == "__main__":
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    from sys import argv
+    Session = sessionmaker()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = Session(bind=engine)
+    Base.metadata.create_all(engine)
+    instances = session.query(State).filter(State.name.like('%a%'))\
+        .order_by(State.id).all()
+    if instances:
+        for instance in instances:
+            if 'a' in instance.name:
+                print("{}: {}".format(instance.id, instance.name))
+    session.close()
