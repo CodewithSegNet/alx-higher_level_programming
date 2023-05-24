079053e2a37209fd4c5499c81fe821e29c38d414
segunPc <eeomo@yahoo.com>
2023-05-24 14:59:10 +0100

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+filePath = process.argv[2];
+
+fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
