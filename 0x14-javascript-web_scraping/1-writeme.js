5227124b19c471c2c20544d40ccbfd12c436e30a
segunPc <eeomo@yahoo.com>
2023-05-24 15:16:46 +0100

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filePath, data, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
